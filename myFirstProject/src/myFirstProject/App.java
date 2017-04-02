621e5f44c350452db1343be8ac6746eb28252d06
Luke Gehring <lukegehring99@gmail.com>
2017-04-01 22:34:58 -0500

    Another output line added to App

--- myFirstProject/src/myFirstProject/App.java
+++ myFirstProject/src/myFirstProject/App.java
@@ -4,6 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("This was me playing with github and getting it to work");
+		System.out.println("This is a change to the program");
 
 	}
 
